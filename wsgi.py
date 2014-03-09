03a3e4f9ef300fd38291ac94924b0b0d0c37884c
Theo Spears <theo@theos.me.uk>
2014-03-09 21:25:42 +0000

    Allow using a non-default DJANGO_SETTINGS_MODULE

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
